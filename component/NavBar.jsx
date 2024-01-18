9bb0985f13af822f6637d14fd40a72a70b118f91
Viktor <onnionboy@gmail.com>
2024-01-18 19:09:52 +0300

    first

--- component/NavBar.jsx
+++ component/NavBar.jsx
@@ -6,32 +6,10 @@ function NavBar({ user }) {
       <div className='container-fluid'>
         <div className='collapse navbar-collapse' id='navbarNav'>
           <ul className='navbar-nav'>
-          <div className='rigth-nav'>
-          <li className='nav-item'>
-              <a className='nav-link active' aria-current='page' href='/'>
-                Main
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='/'>
-               News
-              </a>
-            </li>
-            <li className='nav-item'>
-              <p className='nav-link'></p>
-            </li>
-          </div>
-           <div className='left-nav'>
-           {user ? (
-              <>
+            <div className='rigth-nav'>
               <li className='nav-item'>
                 <a className='nav-link active' aria-current='page' href='/'>
                   Main
-                </a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link' href='/'>
-                  Users
                 </a>
               </li>
               <li className='nav-item'>
