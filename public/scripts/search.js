a2abbccc2632d0082a84520a5998bbb7dc79ad68
Daniil Yudanov <yudanchik.daniil@mail.ru>
2024-01-18 19:05:40 +0300

    Сделал валидацию на клиенте

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -4,19 +4,23 @@ if (searchForm) {
   searchForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const { word, stopword } = e.target;
-    const res = await fetch('/api/word', {
-      method: 'post',
-      headers: { 'content-type': 'application/json' },
-      body: JSON.stringify({ word: word.value, stopword: stopword.value }),
-    });
+    if (word.value.trim() && stopword.value.trim()) {
+      const res = await fetch('/api/word', {
+        method: 'post',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify({ word: word.value, stopword: stopword.value }),
+      });
 
-    const data = await res.json();
-    console.log(data);
-    if (data.message === 'успех') {
-      const news = document.querySelector('.news-cards');
-      news.innerHTML = data.html;
-      return;
+      const data = await res.json();
+      console.log(data);
+      if (data.message === 'успех') {
+        const news = document.querySelector('.news-cards');
+        news.innerHTML = data.html;
+        return;
+      }
+      alert(data.message);
     }
-    alert(data.message);
+    alert('Вы оставили пустым поле:Ключевое слово или поле:Исключить слово');
+    return;
   });
 }
