d14916ea0e5644da51093f44692285981db0bd37
Viktor <onnionboy@gmail.com>
2024-01-18 20:24:42 +0300

    success

--- component/NewsItem.jsx
+++ component/NewsItem.jsx
@@ -1,14 +1,25 @@
 const React = require('react');
 
-function NewsItem({newsOne}) {
+function NewsItem({ newsOne, user }) {
   return (
     <div className='card'>
       <img src={newsOne.img} className='card-img-top' alt={`${newsOne.id}`} />
       <div className='card-body'>
         <h5 className='card-title'>{newsOne.tittle}</h5>
         <a href={`/${newsOne.id}`} className='btn btn-primary'>
-          Подробнее 
+          Подробнее
         </a>
+        {user && user.isAdmin === true && (
+          <>
+            <button
+              data-id={newsOne.id}
+              className='btn btn-danger delete'
+              type='button'
+            >
+              Удалить
+            </button>
+          </>
+        )}
       </div>
     </div>
   );
---
+++ public/scripts/delete.js
@@ -0,0 +1,20 @@
+const deleteNews = document.querySelector('.news-cards');
+console.log(deleteNews);
+if (deleteNews) {
+  deleteNews.addEventListener('click', async (e) => {
+    if (e.target.classList.contains('delete')) {
+      const { id } = e.target.dataset;
+      console.log(id);
+      const res = await fetch(`/api/news/${id}`, {
+        method: 'delete',
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.message === 'success') {
+        e.target.closest('.card').remove();
+      } else {
+        alert(data.message);
+      }
+    }
+  });
+}
