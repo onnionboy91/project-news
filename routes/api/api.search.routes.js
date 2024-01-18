7852d551b95b38692786739f9f84b07273e19a15
Daniil Yudanov <yudanchik.daniil@mail.ru>
2024-01-18 17:00:52 +0300

    Сделал сортировку карточек

--- routes/api/api.search.routes.js
+++ routes/api/api.search.routes.js
@@ -1,30 +1,26 @@
 const router = require('express').Router();
 const { News } = require('../../db/models');
+const { Op } = require('sequelize');
+const HomePage = require('../../component/HomePage');
+const NewsCardsList = require('../../component/NewsCardsList');
 //Db
 
 router.post('/word', async (req, res) => {
   try {
     const { word, stopword } = req.body;
-    const arrNews = await News.findAll();
-    const arrNewsTitleStr = arrNews.map((el) => el.tittle.split(' '));
-    let str = [];
-    arrNewsTitleStr.forEach((el) => {
-      let arrNew = el.filter((element) => element == word);
-      if (arrNew.length > 0) {
-        return str.push(el);
-      }
+    let news = await News.findAll({
+      where: {
+        tittle: {
+          [Op.substring]: word,
+          [Op.notLike]: `%${stopword}%`,
+        },
+      },
     });
-    // console.log(str, '9999999999999999999999999999999999999999'); //получили массив в массиве с правильным title
-    let resultObj = [];
-    for (let i = 0; i < str.length; i++) {
-      const titleNoDb = str[i].join(' ');
-      const obj = await News.findOne({ where: { tittle: titleNoDb } });
-      resultObj.push(obj);
-    }
-
-    // console.log(resultObj, 'ffffffffffffffffffffffffffffff'); // получили три объекта
-
-    res.json({ message: 'успех', resultObj });
+    const html = res.renderComponent(NewsCardsList, {
+      title: 'Home Page',
+      news,
+    });
+    res.json({ message: 'успех', html });
   } catch ({ message }) {
     res.json({ message });
   }
